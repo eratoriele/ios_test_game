c1187d3b8e79814fba01c3ee165b87fc12ae4407
Bora Gulerman <fafadaw1234@gmail.com>
2021-09-07 07:39:16 -0700

    First Commit

---
+++ infinite_runner/enums.swift
@@ -0,0 +1,34 @@
+//
+//  zPosition_enum.swift
+//  infinite_runner
+//
+//  Created by macos on 7.09.2021.
+//  Copyright © 2021 macos. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+
+enum zPositions {
+    
+    static let background: CGFloat = 0
+    static let bullet: CGFloat = 1
+    static let player: CGFloat = 2    
+}
+
+enum playerStartingPosition {
+    
+    static let xMultiplier: CGFloat = 0.5
+    static let yMultiplier: CGFloat = 0.2
+}
+
+enum bulletConstants {
+    
+    static let reAttackTime = 0.3
+    
+    enum direction {
+        case up
+        case down
+    }
+}
